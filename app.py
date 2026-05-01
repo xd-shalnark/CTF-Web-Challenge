de61b078b9782cf4e5a29fa996af60285d74391a
xd-shalnark <voronenko333@icloud.com>
2026-05-01 22:26:31 +0200

    fix close security vulnerabilities

--- app.py
+++ app.py
@@ -15,14 +15,21 @@ def index():
         if key == SECRET_KEY:
             return jsonify({"key": FLAG})
         return jsonify({"error": "Wrong key"}), 403
-    
+
+    if request.method == "GET":
+        user_agent = request.headers.get("User-Agent", "")
+        if "curl" in user_agent.lower():
+            return "Try using -d or --data flag 😉", 200
+
     response = make_response(send_file(os.path.join(BASE_DIR, "mask.html")))
     response.set_cookie("hint", "The key is --server-- and the value is --payload--")
     return response
 
 @app.route('/<path:filename>')
 def serve_static(filename):
-    return send_from_directory(BASE_DIR, filename)
+    if filename.startswith("bildet/"):
+        return send_from_directory(BASE_DIR, filename)
+    return "Nothing here ", 404
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
