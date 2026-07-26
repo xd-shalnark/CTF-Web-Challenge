1e769d036b892150c8c1376cd18116fa8cf08a89
xd-shalnark <243507956+xd-shalnark@users.noreply.github.com>
2026-07-26 22:07:29 +0200

    fix(app.py):fix trace back vulnerability

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def serve_static(filename):
     return "Nothing here ", 404
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
